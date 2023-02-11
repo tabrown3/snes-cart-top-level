8860faa2832119be3bb7359448f7c415269e0540
tabrown3 <t-a-brown@hotmail.com>
2023-02-11 15:11:21 -0600

    add in and out bytes

--- spi_slave.v
+++ spi_slave.v
@@ -3,33 +3,37 @@ module spi_slave
     input clk,
     input spi_clk,
     input mosi,
-    output miso
+    input [7:0] out_byte,
+    output reg miso = 1'b0,
+    output busy,
+    output [7:0] in_byte
 );
-    reg busy = 1'b0;
+
+    reg started = 1'b0;
     reg finished = 1'b0;
     reg [3:0] bit_cnt = 4'h0;
     reg [7:0] in_byte_reg = 8'h00;
 
-    wire [7:0] in_byte;
-
+    assign busy = started ^ finished;
     assign in_byte = in_byte_reg;
 
-    always @(posedge clk) begin
-        if (finished) begin
-            finished <= 1'b0;
-        end
-
-        if (bit_cnt == 4'h0 && spi_clk) begin
-            busy <= 1'b1;
-            bit_cnt <= bit_cnt + 1;
-            in_byte_reg[bit_cnt] <= mosi;
-        end else if (bit_cnt < 8) begin
-            bit_cnt <= bit_cnt + 1;
+    always @(posedge spi_clk) begin
+        if (bit_cnt == 4'h0) begin
+            started <= ~started;
             in_byte_reg[bit_cnt] <= mosi;
         end else begin
-            busy <= 1'b0;
-            bit_cnt <= 4'h0;
-            finished <= 1'b1;
+            in_byte_reg[bit_cnt] <= mosi;
+        end
+    end
+
+    always @(negedge spi_clk) begin
+        if (bit_cnt == 7) begin
+            finished <= started;
+            miso <= out_byte[bit_cnt];
+            bit_cnt <= 0;
+        end else begin
+            miso <= out_byte[bit_cnt];
+            bit_cnt <= bit_cnt + 1;
         end
     end
 endmodule
