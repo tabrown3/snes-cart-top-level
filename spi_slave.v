1dc75106fde41cb8849a1d687fa11ba17a281656
tabrown3 <t-a-brown@hotmail.com>
2023-02-11 15:38:17 -0600

    drive spi_slave with onboard clk

--- spi_slave.v
+++ spi_slave.v
@@ -13,27 +13,35 @@ module spi_slave
     reg finished = 1'b0;
     reg [3:0] bit_cnt = 4'h0;
     reg [7:0] in_byte_reg = 8'h00;
+    // Tracks whether the spi_clk rising transition has been handled,
+    //  since it should only be handled once, and the falling can only
+    //  be handled after the rising.
+    reg posedge_handled = 1'b0;
 
     assign busy = started ^ finished;
     assign in_byte = in_byte_reg;
 
-    always @(posedge spi_clk) begin
-        if (bit_cnt == 4'h0) begin
-            started <= ~started;
-            in_byte_reg[bit_cnt] <= mosi;
-        end else begin
-            in_byte_reg[bit_cnt] <= mosi;
-        end
-    end
+    always @(posedge clk) begin
+        if (spi_clk && !posedge_handled) begin
+            posedge_handled <= 1'b1;
 
-    always @(negedge spi_clk) begin
-        if (bit_cnt == 7) begin
-            finished <= started;
-            miso <= out_byte[bit_cnt];
-            bit_cnt <= 0;
-        end else begin
-            miso <= out_byte[bit_cnt];
-            bit_cnt <= bit_cnt + 1;
+            if (bit_cnt == 4'h0) begin
+                started <= ~started;
+                in_byte_reg[bit_cnt] <= mosi;
+            end else begin
+                in_byte_reg[bit_cnt] <= mosi;
+            end
+        end else if (!spi_clk && posedge_handled) begin
+            posedge_handled <= 1'b0;
+
+            if (bit_cnt == 7) begin
+                finished <= started;
+                miso <= out_byte[bit_cnt];
+                bit_cnt <= 0;
+            end else begin
+                miso <= out_byte[bit_cnt];
+                bit_cnt <= bit_cnt + 1;
+            end
         end
     end
 endmodule
