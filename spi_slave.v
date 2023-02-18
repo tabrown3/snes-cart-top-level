d716b7761fbcab8f8b0324c8886c8ab827009e56
tabrown3 <t-a-brown@hotmail.com>
2023-02-18 17:06:42 -0600

    remove cs usage

--- spi_slave.v
+++ spi_slave.v
@@ -1,6 +1,5 @@
 module spi_slave
 (
-    input cs,
     input spi_clk,
     input mosi,
     input [7:0] out_byte,
@@ -15,25 +14,21 @@ module spi_slave
     assign in_byte = in_byte_reg;
 
     always @(posedge spi_clk) begin
-        if (!cs) begin
-            if (bit_cnt == 4'h7) begin
-                miso <= out_byte[bit_cnt];
-            end else begin
-                miso <= out_byte[bit_cnt];
-            end
+        if (bit_cnt == 4'h7) begin
+            miso <= out_byte[bit_cnt];
+        end else begin
+            miso <= out_byte[bit_cnt];
         end
     end
 
     always @(negedge spi_clk) begin
-        if (!cs) begin
-            if (bit_cnt == 4'h0) begin
-                finished <= ~finished;
-                in_byte_reg[bit_cnt] <= mosi;
-                bit_cnt <= 4'h7;
-            end else begin
-                in_byte_reg[bit_cnt] <= mosi;
-                bit_cnt <= bit_cnt - 1;
-            end
+        if (bit_cnt == 4'h0) begin
+            finished <= ~finished;
+            in_byte_reg[bit_cnt] <= mosi;
+            bit_cnt <= 4'h7;
+        end else begin
+            in_byte_reg[bit_cnt] <= mosi;
+            bit_cnt <= bit_cnt - 1;
         end
     end
 endmodule
