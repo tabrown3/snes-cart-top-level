3ad6710a02dbf6aabe4298af26e0a4f234a0bd22
tabrown3 <t-a-brown@hotmail.com>
2023-02-18 20:15:21 -0600

    create brain file

---
+++ brain.v
@@ -0,0 +1,17 @@
+module brain(
+    input clk,
+    input byte_finished,
+    input frame_finished,
+    input [7:0] cmd,
+    input [7:0] arg1,
+    input [7:0] arg2,
+    input [7:0] checksum_crc8,
+    input [15:0] checksum_crc16,
+    output reg cmd_reset = 1'b0,
+    output reg crc_reset = 1'b0,
+    output reg cmd_en = 1'b1,
+    output reg crc_en = 1'b1,
+    output reg [7:0] out_byte = 8'h00
+);
+
+endmodule
